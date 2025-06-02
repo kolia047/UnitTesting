982d8613c28131f2dc39f954c41caed928662101
Nikolay Pervushin <kolia047@mail.ru>
2025-06-02 10:02:07 +0500

    Test parallel

--- src/dev/hellojava/TestClassCar.java
+++ src/dev/hellojava/TestClassCar.java
@@ -3,12 +3,15 @@ package dev.hellojava;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.*;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestClassCar {
 
     @Test
+    @Execution(ExecutionMode.CONCURRENT)
     //@EnabledOnOs(OS.WINDOWS)
     //@DisabledOnOs(OS.LINUX)
     //@EnabledOnJre(JRE.JAVA_11)
@@ -16,6 +19,13 @@ public class TestClassCar {
     public void getCarYear(){
         Car car = new Car("FJFJJF", "DBC-548", 2020, "I am");
         assertEquals(2020, car.getYear());
+    }
+
+    @Test
+    @Execution(ExecutionMode.CONCURRENT)
+    public void getCarOwner(){
+        Car car = new Car("FJFJJF", "DBC-548", 2020, "I am");
+        assertEquals("I am", car.getOwner());
     }
 
 }
